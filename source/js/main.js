7d5b5718ed4b2912abce194c137dcb3f96f69a0c
01gap <paola.pogli@gmail.com>
2021-01-28 02:45:10 +0300

    добавлен faq

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,10 @@
 (function() {
   const pageHeader = document.querySelector('.page-header');
   const menuButton = pageHeader.querySelector('.page-header__menu-button');
+  const accordionControls = document.querySelectorAll('.accordion__control');
+  const accordionPanels = document.querySelectorAll('.accordion__panel');
+  const PADDING = 15;
+
 
   if (pageHeader !== null && menuButton !== null) {
     pageHeader.classList.remove('page-header--no-js');
@@ -12,4 +16,28 @@
       pageHeader.classList.toggle('page-header--menu-opened');
     });
   }
+
+  accordionPanels.forEach((panel) =>
+    panel.setAttribute('data-height', panel.offsetHeight + PADDING + 'px')
+  )
+
+  accordionPanels.forEach((panel) =>
+    panel.style.height = '0'
+  )
+
+  accordionControls.forEach((control) =>
+    control.addEventListener('click', () => {
+      const parent = control.parentNode;
+      const panel = parent.childNodes[3];
+      let expanded = control.getAttribute('aria-expanded') === 'true';
+      control.setAttribute('aria-expanded', !expanded);
+      if (parent.classList.contains('accordion__item--active')) {
+        panel.style.height = '0';
+        parent.classList.remove('accordion__item--active');
+      } else {
+        panel.style.height =  panel.getAttribute('data-height');
+        parent.classList.add('accordion__item--active');
+      }
+    })
+  )
 })();
