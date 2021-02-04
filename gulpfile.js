6e01e96a5c2b89dfa4e315a4c9c4dbed73eb90b0
01gap <paola.pogli@gmail.com>
2021-02-04 04:19:56 +0300

    добавлен фильтр

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const sass = require("gulp-sass");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const sync = require("browser-sync").create();
+'use strict';
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sync = require('browser-sync').create();
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const svgstore = require('gulp-svgstore');
@@ -12,19 +13,22 @@ const svgstore = require('gulp-svgstore');
 // Styles
 
 const styles = () => {
-  return gulp.src("source/sass/styles.scss")
+  return gulp
+    .src('source/sass/styles.scss')
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer({
-        grid: true,
-      })
-    ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(
+        postcss([
+          autoprefixer({
+            grid: true,
+          }),
+        ])
+    )
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('source/css'))
     .pipe(sync.stream());
-}
+};
 
 exports.styles = styles;
 
@@ -32,14 +36,14 @@ const sprite = () => {
   return gulp
     .src('source/img/icon-{pinterest,fb,insta,twitter,right-flash}.svg')
     .pipe(
-      imagemin([
-        imagemin.svgo({
-          plugins: [
-            { removeViewBox: false },
-            { removeAttrs: { attrs: '(fill)' }}
-          ]
-        })
-      ])
+        imagemin([
+          imagemin.svgo({
+            plugins: [
+              {removeViewBox: false},
+              {removeAttrs: {attrs: '(fill)'}},
+            ],
+          }),
+        ])
     )
     .pipe(svgstore())
     .pipe(rename('sprite.svg'))
@@ -52,24 +56,22 @@ exports.sprite = sprite;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'source',
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 exports.server = server;
 
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
-}
+  gulp.watch('source/sass/**/*.scss', gulp.series('styles'));
+  gulp.watch('source/*.html').on('change', sync.reload);
+};
 
-exports.default = gulp.series(
-  styles, server, watcher
-);
+exports.default = gulp.series(styles, server, watcher);
--- source/js/main.js
+++ source/js/main.js
@@ -1,74 +1,90 @@
-
 'use strict';
-(function() {
-  const PADDING = 15;
-  const pageHeader = document.querySelector('.page-header');
-  const menuButton = pageHeader.querySelector('.page-header__menu-button');
-  const accordionControls = document.querySelectorAll('.accordion__control');
-  const accordionPanels = document.querySelectorAll('.accordion__panel');
-  const body = document.querySelector('body');
-  let bodyOverlay = false;
-
+(function () {
+  var pageHeader = document.querySelector('.page-header');
+  var menuButton = pageHeader.querySelector('.page-header__menu-button');
+  var accordionControls = document.querySelectorAll('.accordion__control');
+  var accordionPanels = document.querySelectorAll('.accordion__panel');
+  var body = document.querySelector('body');
 
   if (pageHeader !== null && menuButton !== null) {
     pageHeader.classList.remove('page-header--no-js');
 
-    menuButton.addEventListener('click', () => {
-      let expanded = menuButton.getAttribute('aria-expanded') === 'true';
+    menuButton.addEventListener('click', function () {
+      var expanded = menuButton.getAttribute('aria-expanded') === 'true';
       menuButton.setAttribute('aria-expanded', !expanded);
       pageHeader.classList.toggle('page-header--menu-opened');
       if (!expanded) {
         body.classList.add('overlay');
       } else {
-       body.classList.remove('overlay');
+        body.classList.remove('overlay');
       }
     });
   }
 
-  accordionPanels.forEach((panel) =>
-    panel.setAttribute('data-height', panel.offsetHeight + PADDING + 'px')
-  )
+  accordionPanels.forEach(function (panel) {
+    return panel.setAttribute('data-height', panel.offsetHeight + 'px');
+  });
 
-  accordionPanels.forEach((panel) =>
-    panel.style.height = '0'
-  )
+  accordionPanels.forEach(function (panel) {
+    var padding = window.getComputedStyle(panel).getPropertyValue('padding-top');
+    return panel.setAttribute('data-padding-top', padding);
+  });
 
-  accordionControls.forEach((control) =>
-    control.addEventListener('click', () => {
-      const parent = control.parentNode;
-      const panel = parent.childNodes[3];
-      let expanded = control.getAttribute('aria-expanded') === 'true';
+  var zeroPadding = function (panel) {
+    panel.style.paddingTop = '0';
+  };
+
+  var zeroHeight = function (panel) {
+    panel.style.height = '0';
+  };
+
+  accordionPanels.forEach(function (panel) {
+    zeroPadding(panel);
+    zeroHeight(panel);
+  });
+
+  var animatePanel = function animatePanel(control) {
+    control.addEventListener('click', function () {
+      var parent = control.parentNode;
+      var panel = parent.childNodes[3];
+      var expanded = control.getAttribute('aria-expanded') === 'true';
       control.setAttribute('aria-expanded', !expanded);
       if (parent.classList.contains('accordion__item--active')) {
         panel.style.height = '0';
+        panel.style.paddingTop = '0';
         parent.classList.remove('accordion__item--active');
       } else {
-        panel.style.height =  panel.getAttribute('data-height');
+        panel.style.height = panel.getAttribute('data-height');
+        panel.style.paddingTop = panel.getAttribute('data-padding-top');
         parent.classList.add('accordion__item--active');
       }
-    })
-  )
+    });
+  };
 
-   // carousel
-  const carousel = new Swiper('.carousel__container', {
+  accordionControls.forEach(function (control) {
+    animatePanel(control);
+  });
+
+  // carousel
+  var swiper = new Swiper('.carousel__container', {
     navigation: {
       prevEl: '.carousel__button-prev',
-      nextEl: '.carousel__button-next',
+      nextEl: '.carousel__button-next'
     },
 
     pagination: {
       el: '.swiper__pagination',
       clickable: true,
-      renderBullet: function (index, className) {
+      renderBullet: function renderBullet(index, className) {
         return '<span class="' + className + '">' + (index + 1) + '</span>';
-      },
+      }
     },
     // grabCursor: true,
     keyboard: {
-      enabled: true,
+      enabled: true
     },
     mousewheel: {
-      sensitivity: 1,
+      sensitivity: 1
     },
     // slidesPerView: 4,
     watchOverflow: true,
@@ -80,7 +96,7 @@
     breakpoints: {
       320: {
         slidesPerView: 2,
-        slidesPerGroup: 2,
+        slidesPerGroup: 2
       },
       // 768: {
       //   slidesPerView: 3,
@@ -88,36 +104,22 @@
       // },
       1024: {
         slidesPerView: 4,
-        slidesPerGroup: 4,
-      }
-    }
+        slidesPerGroup: 4
+      },
+    },
   });
-
-
-  // if (mQueryTablet.matches) {
-  //   // let slidesFirst = carousel.slides;
-  //   carousel.removeSlide([2, 3, 6, 7, 10, 11]);
-  //   // let slides = carousel.slides;
-  //   // carousel.appendSlide([carousel.slides[0], carousel.slides[1]]);
-  //   // let slides = document.querySelectorAll('.carousel__slide');
-  //   // const wrapper = document.querySelector('.carousel__slide-wrapper');
-  //   // wrapper.removeChild(wrapper.childNodes[2, 3]);
-  //   // wrapper.appendChild(wrapper.childNodes[0, 1]);
-  //   console.log(slides.length);
-  // }
-
-
-
 })();
 
+
 // 'use strict';
-// (function() {
+// (function () {
+//   const PANEL_PADDING = 15;
 //   const pageHeader = document.querySelector('.page-header');
 //   const menuButton = pageHeader.querySelector('.page-header__menu-button');
 //   const accordionControls = document.querySelectorAll('.accordion__control');
 //   const accordionPanels = document.querySelectorAll('.accordion__panel');
-//   const PADDING = 15;
-
+//   const body = document.querySelector('body');
+//   // let bodyOverlay = false;
 
 //   if (pageHeader !== null && menuButton !== null) {
 //     pageHeader.classList.remove('page-header--no-js');
@@ -126,42 +128,19 @@
 //       let expanded = menuButton.getAttribute('aria-expanded') === 'true';
 //       menuButton.setAttribute('aria-expanded', !expanded);
 //       pageHeader.classList.toggle('page-header--menu-opened');
+//       if (!expanded) {
+//         body.classList.add('overlay');
+//       } else {
+//         body.classList.remove('overlay');
+//       }
 //     });
 //   }
 
-//   // accordionPanels.forEach((panel) =>
-//   //   panel.setAttribute('data-height', panel.offsetHeight + PADDING + 'px')
-//   // )
+//   accordionPanels.forEach((panel) => panel.setAttribute('data-height', panel.offsetHeight + PANEL_PADDING + 'px'));
 
-//   // accordionPanels.forEach((panel) =>
-//   //   panel.setAttribute('data-height', panel.offsetHeight)
-//   // )
+//   accordionPanels.forEach((panel) => (panel.style.height = '0'));
 
-//   const setPanelAttributes = (elem) => {
-//     elem.setAttribute('data-height', elem.offsetHeight);
-//     const paddingTop = window.getComputedStyle(elem, null).getPropertyValue('padding-top').slice(0, -2);
-//     elem.setAttribute('data-padding-top', paddingTop);
-//   };
-
-//   accordionPanels.forEach((panel) =>
-//     setPanelAttributes(panel)
-//   )
-
-//   const hidePanel = (elem) => {
-//     elem.style.height = '0';
-//   };
-
-//   accordionPanels.forEach((panel) =>
-//     hidePanel(panel)
-//   )
-
-//   // accordionPanels.forEach((panel) =>
-//   //   panel.setAttribute('data-padding-top', window.getComputedStyle(panel, null).getPropertyValue('padding-top').slice(0, -2))
-//   // )
-
-
-
-//   accordionControls.forEach((control) =>
+//   const animatePanel = (control) => {
 //     control.addEventListener('click', () => {
 //       const parent = control.parentNode;
 //       const panel = parent.childNodes[3];
@@ -171,11 +150,55 @@
 //         panel.style.height = '0';
 //         parent.classList.remove('accordion__item--active');
 //       } else {
-//           parent.classList.add('accordion__item--active');
-//           const padding = parseInt(panel.getAttribute('data-padding-top'));
-//           const height = parseInt(panel.getAttribute('data-height'));
-//           panel.style.height =  height + padding + 'px';
+//         panel.style.height = panel.getAttribute('data-height');
+//         parent.classList.add('accordion__item--active');
 //       }
-//     })
-//   )
+//     });
+//   };
+
+//   accordionControls.forEach((control) => animatePanel(control));
+
+//   // carousel
+//   new Swiper('.carousel__container', {
+//     navigation: {
+//       prevEl: '.carousel__button-prev',
+//       nextEl: '.carousel__button-next',
+//     },
+
+//     pagination: {
+//       el: '.swiper__pagination',
+//       clickable: true,
+//       renderBullet: function (index, className) {
+//         return '<span class="' + className + '">' + (index + 1) + '</span>';
+//       },
+//     },
+//     // grabCursor: true,
+//     keyboard: {
+//       enabled: true,
+//     },
+//     mousewheel: {
+//       sensitivity: 1,
+//     },
+//     // slidesPerView: 4,
+//     watchOverflow: true,
+//     spaceBetween: 30,
+//     // slidesPerGroup: 4,
+//     // loop: true,
+//     observer: true,
+//     // loopAdditionalSlides: 4,
+//     breakpoints: {
+//       320: {
+//         slidesPerView: 2,
+//         slidesPerGroup: 2,
+//       },
+//       // 768: {
+//       //   slidesPerView: 3,
+//       //   slidesPerGroup: 3,
+//       // },
+//       1024: {
+//         slidesPerView: 4,
+//         slidesPerGroup: 4,
+//       },
+//     },
+//   });
 // })();
